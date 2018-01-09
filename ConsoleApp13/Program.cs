39c3844e7aefb328f0812d76721908184be0e687
Озик <djozkh@gmail.com>
2018-01-09 16:21:45 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summ = 1.0;
+            var percent = 0.03;
+            var score = 1.0;
+
+            for (var i = 0; i < 2000; i++)
+            {
+                score =  score * percent + score;
+
+            }
+            Console.WriteLine(score);
+
+
+        }
+    }
+}
